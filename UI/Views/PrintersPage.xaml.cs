f6cc16d3eb52e3b5760ab1a2525e4dbd5f3df9bf
JUAN SEBASTIAN CAMACHO MUÑOZ <jscamacho@unicesar.edu.co>
2026-02-28 19:29:06 -0500

    globalizacion de colores dinamicos

--- UI/Views/PrintersPage.xaml.cs
+++ UI/Views/PrintersPage.xaml.cs
@@ -18,5 +18,40 @@ namespace UI.Views
                 viewModel.LoadPrintersCommand.Execute(null);
             }
         }
+
+        // --- Lógica manual para el Hover de los íconos de Acción (Edit/Delete) ---
+        // Dado que MAUI VisualStateManager ignora la directiva Setter TargetName="" dentro de DataTemplates, 
+        // lo controlamos manualmente inyectando eventos Pointer.
+
+        private void OnCardPointerEntered(object sender, PointerEventArgs e)
+        {
+            // Solo en Windows/Mac ocultamos iconos por defecto y los mostramos en hover
+#if WINDOWS || MACCATALYST
+            if (sender is Border border)
+            {
+                // Buscar el StackLayout de acciones ("ActionIcons")
+                var actionIcons = border.FindByName<HorizontalStackLayout>("ActionIcons");
+                if (actionIcons != null)
+                {
+                    // Transición suave
+                    actionIcons.FadeTo(1, 150);
+                }
+            }
+#endif
+        }
+
+        private void OnCardPointerExited(object sender, PointerEventArgs e)
+        {
+#if WINDOWS || MACCATALYST
+            if (sender is Border border)
+            {
+                var actionIcons = border.FindByName<HorizontalStackLayout>("ActionIcons");
+                if (actionIcons != null)
+                {
+                    actionIcons.FadeTo(0, 150);
+                }
+            }
+#endif
+        }
     }
 }
